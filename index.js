c33fbb02fddb90e4012d4e59bacd2c7e1c2267a7
Zhark10 <arkady.zharavin@citronium.biz>
2020-06-06 23:56:03 +0300

    minor changes

--- index.js
+++ index.js
@@ -3,12 +3,40 @@ const app = express();
 const PORT = process.env.PORT || 80;
 
 app.get("/", (req, res) => {
-  res.end("<h1>Home page</h1>");
+  res.end(`
+   <div>
+        <nav>
+        <ul>
+            <li>
+                <a href="/">Home</a>
+            </li>
+            <li>
+                <a href="/about">About</a>
+            </li>
+        </ul>
+        </nav>
+    <h1>Home page</h1>
+  </div>
+  `);
 });
 
 app.get("/about", (req, res) => {
-    res.end("<h1>About page</h1>");
-  });
+  res.end(`
+   <div>
+        <nav>
+        <ul>
+            <li>
+                <a href="/">Home</a>
+            </li>
+            <li>
+                <a href="/about">About</a>
+            </li>
+        </ul>
+        </nav>
+    <h1>About page</h1>
+  </div>
+  `);
+});
 
 app.listen(PORT, () => {
   console.log("server has been started");
